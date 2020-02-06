8282fdee795d0a1b4bf6657fb3071f6f9cdd4c43
anarahe <anarahe@gmail.com>
2020-02-06 14:20:31 +0100

    Tutorial con CAT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Param } from '@nestjs/common';
+import { AppService, CatsService } from './app.service';
 
 @Controller()
 export class AppController {
@@ -9,4 +9,38 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+}
+
+@Controller('cats')
+export class CatsController {
+
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+
+/*   @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  } */
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `This action returns a #${id} cat`;
+  }
+
+
+  @Get('ab*cd')
+  findAllAbcd() {
+    return 'This route uses a wildcard';
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -6,3 +6,22 @@ export class AppService {
     return 'AppService - Hello World!';
   }
 }
+
+@Injectable()
+export class CatsService {
+  private readonly cats: Cat[] = [];
+
+  create(cat: Cat) {
+    this.cats.push(cat);
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
+}
+
+export interface Cat {
+  name: string;
+  age: number;
+  breed: string;
+}
